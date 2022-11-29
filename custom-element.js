52a362f3c6155e8a0e101bac3a0291f127fc15c5
user126179663 <115039826+user126179663@users.noreply.github.com>
2022-11-30 02:31:30 +0900

    Add files via upload

--- custom-element.js
+++ custom-element.js
@@ -8,18 +8,14 @@ class CustomElement extends HTMLElement {
 				link = document.createElement('link'),
 				article = document.createElement('article');
 		
-		this.attachShadow({ mode: 'open' }),
-		
 		link.rel = 'stylesheet',
 		link.href = 'shadow.css',
-		
-		root.id = 'root',
 		
 		article.appendChild(document.createElement('div')),
 		
 		root.append(document.createElement('header'), article, document.createElement('footer')),
 		
-		this.shadowRoot.append(link, root)
+		this.attachShadow({ mode: 'open' }).append(link, root)
 		
 	}
 	
